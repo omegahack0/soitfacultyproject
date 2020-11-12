2e36e7a9a2a11376be73fe3654b7961340ccf71e
Matt <you@example.com>
2020-11-12 14:30:26 -0500

    adding save method to controller

--- src/main/java/com/soit/soitfaculty/FacultyController.java
+++ src/main/java/com/soit/soitfaculty/FacultyController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.soit.soitfaculty.entity.Faculty;
@@ -43,6 +45,14 @@ public class FacultyController {
 		theModel.addAttribute("faculty", theFaculty);
 		
 		return "faculties/faculty-Form";	
+	}
+	@PostMapping("/save")
+	public String saveFaculty(@ModelAttribute("faculty") Faculty theFaculty) {
+		//Register the Faculty member
+		facultyService.save(theFaculty);
+		
+		//Using Redirect to prevent double submission
+		return "redirect:/Faculties/list";
 	}
 	
 }
